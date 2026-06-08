b7d07891ad6f6f1ddccbad75a86998d9594f941c
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-06-08 17:23:49 +0300

    feat(format): setup conform.nvim plugin

---
+++ lua/custom/plugins/formatter.lua
@@ -0,0 +1,83 @@
+--[ Formatting
+
+----[ Install plugin and dependencies
+
+---@type (string|vim.pack.Spec)[]
+local source_list = {
+    'https://github.com/stevearc/conform.nvim',
+}
+
+vim.pack.add(source_list)
+
+----[ Configuration
+
+local conform_opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+        -- You can specify filetypes to autoformat on save here:
+        local enabled_filetypes = {
+            lua = true,
+        }
+        if (not vim.g.disable_autoformat or not vim.b[bufnr].disable_autoformat)
+            or enabled_filetypes[vim.bo[bufnr].filetype] then
+            return { timeout_ms = 500 }
+        else
+            return nil
+        end
+    end,
+    default_format_opts = {
+        lsp_format = 'fallback', -- Use external formatters if configured below, otherwise use LSP formatting. Set to `false` to disable LSP formatting entirely.
+    },
+    -- You can also specify external formatters in here.
+    formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially.
+        -- You can use 'stop_after_first' to run the first available formatter from the list.
+        go = { 'gci', 'golines', 'gofumpt', stop_after_first = false },
+        c = { 'clang-format' },
+        -- json = { 'fixjson' },
+        -- yaml = { 'prettier' },
+        -- markdown = { 'prettier' },
+        -- ['*'] = { 'codespell' },
+    },
+    formatters = {
+        gci = {
+            inherit = false,
+            command = 'gci',
+            stdin = false,
+            args = {
+                'write',
+                '--skip-generated',
+                '--skip-vendor',
+                '--custom-order',
+                '-s',
+                'standard',
+                '-s',
+                'default',
+                -- '-s',
+                -- 'blank',
+                -- '-s',
+                -- 'dot',
+                -- '-s',
+                -- 'alias',
+                '-s',
+                'localmodule',
+                '$FILENAME',
+            },
+        },
+        golines = {
+            prepend_args = { '-m', '140' },
+        },
+        ['clang-format'] = {
+            prepend_args = { '--fallback-style=LLVM' },
+        },
+    },
+}
+
+vim.keymap.set({ 'n', 'v' }, '<Leader>fm', function()
+    require('conform').format({ async = true })
+end, { desc = 'Format file or range via conform.nvim' })
+
+----[ Setup
+
+require('conform').setup(conform_opts)
--- lua/custom/usercmd.lua
+++ lua/custom/usercmd.lua
@@ -7,3 +7,18 @@ vim.api.nvim_create_user_command('GitBlameLine', function()
     local filename = vim.api.nvim_buf_get_name(0)
     print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
 end, { desc = 'Print the git blame for the current line' })
+
+-- See: <https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save>
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, { desc = 'Disable autoformat-on-save', bang = true })
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, { desc = 'Re-enable autoformat-on-save' })
