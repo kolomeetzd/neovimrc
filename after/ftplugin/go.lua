da3669d483f0efa66b53f36cbecc80a4fc407d21
Daniel Kolomeetz <kolomeetz.dany@yandex.ru>
2026-06-14 22:39:15 +0300

    config(golang): set indentation rules

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,9 @@
+local go_opts = {
+    expandtab = false,
+    tabstop = 4,
+    shiftwidth = 4,
+}
+
+for name, val in pairs(go_opts) do
+    vim.api.nvim_set_option_value(name, val, { scope = 'local' })
+end
